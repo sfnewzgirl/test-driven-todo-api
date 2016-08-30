2cf06cc57265f6c16e123b5df36e2ea6100ae4f6
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-30 16:13:00 -0700

    completed test driven todo

--- server.js
+++ server.js
@@ -47,6 +47,15 @@ app.get('/api/todos/search', function search(req, res) {
   /* This endpoint responds with the search results from the
    * query in the request. COMPLETE THIS ENDPOINT LAST.
    */
+   var searchTerms = req.query.q;
+   var searchResults = [];
+   for(var i=0; i<todos.length; i++) {
+       if (todos[i].task == searchTerms) {
+        searchResults.push(todos[i]);
+      }
+     }
+  var newTodosList = {todos : searchResults};
+  res.json(newTodosList);
 });
 
 app.get('/api/todos', function index(req, res) {
@@ -59,11 +68,20 @@ app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   function getNewID() {
+     var lastID = todos[todos.length-1]._id;
+     lastID++;
+     return lastID;
+    }
    var task = req.body.task;
    var desc = req.body.description;
-   var newTodo = {task: task, description: desc};
+   var newTodo = {
+     _id: getNewID(),
+      task: task,
+     description: desc
+   };
    todos.push(newTodo);
-   res.jason(todos);
+   res.json(newTodo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
@@ -84,6 +102,17 @@ app.put('/api/todos/:id', function update(req, res) {
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+   var updateTask = req.body.task;
+   var updateDesc = req.body.description;
+   var id = req.params.id;
+   for (var i=0; i < todos.length; i++) {
+       if (todos[i]._id == id) {
+         todos[i].task = updateTask;
+         todos[i].description = updateDesc;
+         var updatedTodo = todos[i];
+       }
+     }
+  res.json(updatedTodo);
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
@@ -91,6 +120,14 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+   var id = req.params.id;
+   for (var i=0; i < todos.length; i++) {
+       if (todos[i]._id == id) {
+         var result = i;
+       }
+     }
+   todos.splice(result, 1);
+   res.json(todos);
 });
 
 /**********
